102aea190d7d049e807ee6372d7b7a8716ec7709
Uncle88 <andreyproskura88@gmail.com>
2017-05-10 15:12:33 +0300

    created model

---
+++ TwitterNewsCollection/TwitterNewsCollection/Models/TwitterTricks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TwitterNewsCollection.Models
+{
+    public class TwitterTricks
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+        public string ImageUri { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
