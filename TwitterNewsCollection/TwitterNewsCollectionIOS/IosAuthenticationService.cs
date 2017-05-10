195a7c68b23935b89a92e7a577fd94926754b3d6
Uncle88 <andreyproskura88@gmail.com>
2017-05-10 14:25:26 +0300

    LoginToTwitter service return obj in JSON format

--- TwitterNewsCollection/TwitterNewsCollectionIOS/IosAuthenticationService.cs
+++ TwitterNewsCollection/TwitterNewsCollectionIOS/IosAuthenticationService.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 using TwitterNewsCollection.Authentication;
 using UIKit;
 using Xamarin.Auth;
@@ -7,32 +8,82 @@ namespace TwitterNewsCollectionIOS
 {
     public class IosAuthenticationService : IAuthenticationService
     {
+
+        //public void LoginToTwitter()
+        //{
+        //    var auth = new OAuth1Authenticator(
+        //        consumerKey: "ZYAaM25koQg2Jdkm1CrSPvCl4",
+        //        consumerSecret: "lgc4bXD3u7AYe9xgwbtgkTincbJR9Ed4oSTJ9H09bMf4RPa9FB",
+        //        requestTokenUrl: new Uri("https://api.twitter.com/oauth/request_token"),
+        //        authorizeUrl: new Uri("https://api.twitter.com/oauth/authorize"),
+        //        accessTokenUrl: new Uri("https://api.twitter.com/oauth/access_token"),
+        //        callbackUrl: new Uri("http://app.home.com"));
+
+
+        //    //var auth = new OAuth1Authenticator(
+        //    //Consumer_KEY,
+        //    // Consumer_SECRET,
+        //    //new Uri(TWITTER_REQTOKEN_URL),
+        //    //new Uri(TWITTER_AUTH),
+        //    //new Uri(TWITTER_ACCESSTOKEN_URL),
+        //    //new Uri(TWITTER_CALLBACK_URL));
+
+        //    auth.AllowCancel = true;
+
+        //    auth.Completed += (s, eventArgs) =>
+        //    {
+        //        if (eventArgs.IsAuthenticated)
+        //        {
+        //            var request = new OAuth1Request("GET", new Uri("https://api.twitter.com/1.1/account/verify_credentials.json"), null, eventArgs.Account, false);
+        //            request.GetResponseAsync().ContinueWith(t =>
+        //                                {
+        //                                    if (t.IsFaulted)
+        //                                    {
+        //                                        UIAlertView alert = new UIAlertView();
+        //                                        alert.Message = t.Exception.InnerException.Message;
+        //                                        alert.Show();
+        //                                        return;
+        //                                    }
+        //                                    else
+        //                                    {
+        //                                        string _data = t.Result.GetResponseText();
+        //                                        //object obj = JsonConvert.DeserializeObject<object>(_data);
+        //                                    }
+        //                                });
+        //            return;
+        //        }
+        //        else
+        //        {
+        //            UIAlertView alert = new UIAlertView()
+        //            {
+        //                Title = "Error",
+        //                Message = "Not Authenticated"
+        //            };
+        //            alert.AddButton("OK");
+        //            alert.Show();
+        //            return;
+        //        }
+        //    };
+
+        //    var authView = (UIKit.UIViewController)auth.GetUI();
+        //    var window = UIApplication.SharedApplication.KeyWindow;
+        //    var vc = window.RootViewController;
+        //    vc.PresentViewController(authView, true, null);
+
+        //}
+
         public void LoginToTwitter()
         {
             var auth = new OAuth1Authenticator(
-            consumerKey: "7xxNH3hQXahsT65rhaDItvL15", // For Twitter login, for configure refer http://www.c-sharpcorner.com/article/register-identity-provider-for-new-oauth-application/  
-consumerSecret: "Ggu9DCwRt6QPTeFzEoAt5R5KHTZT3cuA0lCKgnhFZUidVMJJtf", // For Twitter login, for configure refer http://www.c-sharpcorner.com/article/register-identity-provider-for-new-oauth-application/  
-requestTokenUrl: new Uri("https://api.twitter.com/oauth/request_token"), // These values do not need changing  
-authorizeUrl: new Uri("https://api.twitter.com/oauth/authorize"), // These values do not need changing  
-accessTokenUrl: new Uri("https://api.twitter.com/oauth/access_token"), // These values do not need changing  
-                callbackUrl: new Uri("http://mobile.twitter.com"));//h Set this property to the location the user will be redirected too after successfully authenticating )
+                consumerKey: "ZYAaM25koQg2Jdkm1CrSPvCl4",
+                consumerSecret: "lgc4bXD3u7AYe9xgwbtgkTincbJR9Ed4oSTJ9H09bMf4RPa9FB",
+                requestTokenUrl: new Uri("https://api.twitter.com/oauth/request_token"), // the redirect URL for the service
+                authorizeUrl: new Uri("https://api.twitter.com/oauth/authorize"), // the auth URL for the service
+                accessTokenUrl: new Uri("https://api.twitter.com/oauth/access_token"),
+                callbackUrl: new Uri("https://mobile.twitter.com/home")
+            );
 
-
-            //var auth = new OAuth1Authenticator(
-            //Consumer_KEY,
-            // Consumer_SECRET,
-            //new Uri(TWITTER_REQTOKEN_URL),
-            //new Uri(TWITTER_AUTH),
-            //new Uri(TWITTER_ACCESSTOKEN_URL),
-            //new Uri(TWITTER_CALLBACK_URL));
-
-            auth.AllowCancel = true;
-
-            UIViewController authView = (UIKit.UIViewController)auth.GetUI();
-            var window = UIApplication.SharedApplication.KeyWindow;
-            var vc = window.RootViewController;
-            vc.PresentViewController(authView, true, null);
-            auth.Completed += (s, eventArgs) =>
+            auth.Completed += (sender, eventArgs) =>
             {
                 if (eventArgs.IsAuthenticated)
                 {
@@ -49,7 +100,7 @@ accessTokenUrl: new Uri("https://api.twitter.com/oauth/access_token"), // These 
                                             else
                                             {
                                                 string _data = t.Result.GetResponseText();
-                                                //object obj = JsonConvert.DeserializeObject<object>(_data);
+                                                object obj = JsonConvert.DeserializeObject<object>(_data);
                                             }
                                         });
                     return;
@@ -66,6 +117,13 @@ accessTokenUrl: new Uri("https://api.twitter.com/oauth/access_token"), // These 
                     return;
                 }
             };
+
+            var authView = (UIKit.UIViewController)auth.GetUI();
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var vc = window.RootViewController;
+            vc.PresentViewController(authView, true, null);
         }
     }
+
 }
+
