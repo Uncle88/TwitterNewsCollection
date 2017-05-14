c5b054921ee18a3ca01960e6b6f5a392f13b7636
Uncle88 <andreyproskura88@gmail.com>
2017-05-14 17:36:36 +0300

    Bindings for collection cells

--- TwitterNewsCollection/TwitterNewsCollection/ViewModels/ListViewModel.cs
+++ TwitterNewsCollection/TwitterNewsCollection/ViewModels/ListViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
 using TwitterNewsCollection.Models;
+using MvvmCross.Binding.BindingContext;
 
 namespace TwitterNewsCollection.ViewModels
 {
     public class ListViewModel : MvxViewModel<RootObject>
     {
+        List<RootObject> _rootObj = new List<RootObject>();
+
         public override Task Initialize(RootObject obj)
         {
-            return null;
+            return null;//ShowViewModel();
+            //return null;
         }
     }
 }
