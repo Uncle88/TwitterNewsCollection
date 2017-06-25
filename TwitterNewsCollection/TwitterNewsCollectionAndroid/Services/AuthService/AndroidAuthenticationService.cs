5fec587a7799e409e1a448a800170f130b980251
Uncle88 <andreyproskura88@gmail.com>
2017-06-25 22:17:13 +0300

    send request for var request = new OAuth1Request

--- TwitterNewsCollection/TwitterNewsCollectionAndroid/Services/AuthService/AndroidAuthenticationService.cs
+++ TwitterNewsCollection/TwitterNewsCollectionAndroid/Services/AuthService/AndroidAuthenticationService.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Linq;
+using Android.App;
+using Newtonsoft.Json;
+using TwitterNewsCollection;
 using TwitterNewsCollection.Authentication;
 using TwitterNewsCollection.Helpers;
+using TwitterNewsCollection.Models;
+using Xamarin.Auth;
 
 namespace TwitterNewsCollectionAndroid.Services.AuthService
 {
@@ -10,7 +16,53 @@ namespace TwitterNewsCollectionAndroid.Services.AuthService
 
         public void LoginToTwitter()
         {
-            
+            var auth = new OAuth1Authenticator(
+                Constants.Consumer_KEY,
+                Constants.Consumer_SECRET,
+                new Uri(Constants.TWITTER_REQTOKEN_URL),
+                new Uri(Constants.TWITTER_AUTH),
+                new Uri(Constants.TWITTER_ACCESSTOKEN_URL),
+                new Uri(Constants.TWITTER_CALLBACK_URL));
+
+            auth.Completed += OnAuthCompleted;
+
+            var intent = auth.GetUI(Application.Context);
+            intent.AddFlags(Android.Content.ActivityFlags.NewTask);
+            Application.Context.StartActivity(intent);
+        }
+
+        private async void OnAuthCompleted(object sender, AuthenticatorCompletedEventArgs eventArgs)
+			{
+				if (eventArgs.IsAuthenticated)
+				{
+					var request = new OAuth1Request("GET", new Uri("https://api.twitter.com/1.1/statuses/user_timeline.json"), null, eventArgs.Account);
+					var response = await request.GetResponseAsync();
+					if (response != null)
+					{
+						string _data = response.GetResponseText();
+						var myObj = (TwitterNewsResponse[])JsonConvert.DeserializeObject(_data, typeof(TwitterNewsResponse[]));
+						TwitterEventArgs twEventArgs = new TwitterEventArgs(myObj.ToList());
+						ResponseFeedsCompleted?.Invoke(this, twEventArgs);
+					}
+					else
+					{
+						//UIAlertView alert = new UIAlertView();
+						//alert.Message = "response not received";
+						//alert.Show();
+						//return;
+					}
+				}
+				else
+				{
+					//UIAlertView alert = new UIAlertView()
+					//{
+					//	Title = "Error",
+					//	Message = "Not Authenticated"
+					//};
+					//alert.AddButton("OK");
+					//alert.Show();
+					//return;
+				}
+			}
         }
     }
-}
--- TwitterNewsCollection/TwitterNewsCollectionAndroid/Views/TwitterCollectionView.cs
+++ TwitterNewsCollection/TwitterNewsCollectionAndroid/Views/TwitterCollectionView.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using Android.App;
 using MvvmCross.Droid.Views;
+using TwitterNewsCollectionAndroid.Services.AuthService;
 
 namespace TwitterNewsCollectionAndroid.Views
 {
 [Activity(Label = "Twitter", MainLauncher = true)]
 	public class TwitterCollectionView : MvxActivity
 	{
-		protected override void OnViewModelSet()
+        protected override void OnViewModelSet()
 		{
             SetContentView(Resource.Layout.CollectionView);
+			var ser = new AndroidAuthenticationService();
+			ser.ResponseFeedsCompleted += Ser_ResponseFeedsCompleted;
+			ser.LoginToTwitter();
 		}
-	}
+
+        protected override void OnCreate(Android.OS.Bundle bundle)
+        {
+            base.OnCreate(bundle);
+			SetContentView(Resource.Layout.CollectionView);
+			var ser = new AndroidAuthenticationService();
+			ser.ResponseFeedsCompleted += Ser_ResponseFeedsCompleted;
+			ser.LoginToTwitter();
+        }
+
+        void Ser_ResponseFeedsCompleted(object sender, TwitterNewsCollection.Helpers.TwitterEventArgs e)
+        {
+
+        }
+    }
 }
 
