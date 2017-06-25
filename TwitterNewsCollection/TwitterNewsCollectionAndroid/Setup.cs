f36d43f59b27e75bed01b8ac6a00ff05027d3434
Uncle88 <andreyproskura88@gmail.com>
2017-06-25 16:36:06 +0300

    created CollectionView.xml

--- TwitterNewsCollection/TwitterNewsCollectionAndroid/Setup.cs
+++ TwitterNewsCollection/TwitterNewsCollectionAndroid/Setup.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using Android.Content;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Platform;
+using TwitterNewsCollection;
+
 namespace TwitterNewsCollectionAndroid
 {
-    public class Setup
-    {
-        public Setup()
-        {
-        }
-    }
+		public class Setup : MvxAndroidSetup
+		{
+			public Setup(Context applicationContext)
+			: base(applicationContext)
+			{
+			}
+
+			protected override IMvxApplication CreateApp()
+			{
+				return new App();
+			}
+		}
 }
