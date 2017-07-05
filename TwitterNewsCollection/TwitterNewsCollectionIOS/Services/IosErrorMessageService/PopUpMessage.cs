45c59e99e82b71668268fd34f40991237f57dcea
Uncle88 <andreyproskura88@gmail.com>
2017-07-05 13:40:46 +0300

    created native implement popUpMessage in ios

---
+++ TwitterNewsCollection/TwitterNewsCollectionIOS/Services/IosErrorMessageService/PopUpMessage.cs
@@ -0,0 +1,33 @@
+﻿using TwitterNewsCollection.Services.ErrorMessageService;
+using UIKit;
+
+namespace TwitterNewsCollectionIOS.Services.IosErrorMessageService
+{
+    public class PopUpMessage : IPopUpMessage
+    {
+		private const string errorResponseMessage = "response not received";
+		private const string errorAuthMessage = "Not Authenticated";
+		private const string errorTitle = "Error";
+		private const string buttonText = "OK";
+
+        public void ShowMessageNotAuth()
+        {
+			UIAlertView alert = new UIAlertView()
+			{
+				Title = errorTitle,
+				Message = errorAuthMessage
+			};
+			alert.AddButton(buttonText);
+			alert.Show();
+			return;
+        }
+
+        public void ShowMessageNotResponse()
+        {
+			UIAlertView alert = new UIAlertView();
+			alert.Message = errorResponseMessage;
+			alert.Show();
+			return;
+        }
+    }
+}
