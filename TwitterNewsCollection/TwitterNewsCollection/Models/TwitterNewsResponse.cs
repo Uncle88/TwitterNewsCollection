db85f2f495ce5249db0959939d6ee725670b80ec
Uncle88 <andreyproskura88@gmail.com>
2017-07-26 15:03:07 +0300

    One file- one class.Left one class in the file

---
+++ TwitterNewsCollection/TwitterNewsCollection/Models/TwitterNewsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwitterNewsCollection.Models
+{
+	public class TwitterNewsResponse
+	{
+		public List<RetwittedItem> Data { get; set; }
+	}
+}
